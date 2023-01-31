02408575d4211d31e15a0ef897fbc9ecc7a02161
Martha Huaman <ms.huamann@gmail.com>
2023-01-30 19:00:54 -0500

    (WIP)ADD: data from api

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const BASE_URI = "https://randomuser.me/api/";
+
+export function getUsersData(num) {
+  return fetch(BASE_URI + "?page=1&results=" + num + "&seed=abc").then((response) =>
+    response.json()
+  );
+}
+
